d628fec051cbf9d189038fca0864973d7137f1a2
jiwonman <kusa1230@naver.com>
2022-01-27 14:03:12 +0900

    Google page

---
+++ google/src/pages/main.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../style/main.css'
+
+function main() {
+  return (
+    <div className='container'>
+      <div className='item'><span>C</span><span>H</span><span>A</span><span>N</span><span>Z</span><span>Z</span></div>
+      <div className='item'><input type="text" placeholder='Google'/></div>
+    </div>
+  )
+
+}
+
+export default main
